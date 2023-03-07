2c4a881c49ca61750cf21e21bb9f1b650c6abb07
Gabriel Getzie <ggetzie@hku.hk>
2023-03-07 15:06:16 +0800

    autoformatting

--- src/constants/Axios.tsx
+++ src/constants/Axios.tsx
@@ -1,11 +1,12 @@
-import axios from "axios";
+import axios from 'axios';
 
-// export const baseURL = "https://j20200007.kotsf.com";
-export const baseURL = "http://gpuserver.edu.hku.hk/asl";
+export const baseURL = 'https://j20200007.kotsf.com/asl/';
+// export const baseURL = "http://gpuserver.edu.hku.hk/asl";
 // Test backend url to check for jwt authorization handling from one of my old projects
 // axios.defaults.baseURL = "https://ser.talli.dev/";
 // axios.defaults.baseURL = "https://j20200007.kotsf.com/";
 
-axios.defaults.baseURL = "http://gpuserver.edu.hku.hk/asl/";
+// axios.defaults.baseURL = 'http://gpuserver.edu.hku.hk/asl/';
+axios.defaults.baseURL = 'https://j20200007.kotsf.com/asl/';
 
 // Can add any interceptors here, like for jwt tokens
